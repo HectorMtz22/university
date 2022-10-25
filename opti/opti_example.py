69c42ff55005af367e5590df8f88efbdb4707cc3
HectorMtz22 <hec.gre81@gmail.com>
2022-10-25 08:22:04 -0500

    Function for calculate between 2 variables

---
+++ opti/opti_example.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number of variables: "))
+x = int(input("Ingresa el valor de x:"))
+y = int(input("Ingresa el valor de y:"))
+function = (0.07 * x) + (0.09 * y) 
+
+print("El valor es:", function)
