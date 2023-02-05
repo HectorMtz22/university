46e341dca45e18e556d743bd9bb80eb6f3c0f1d6
Hector Martinez <hec.gre81@gmail.com>
2023-02-05 16:25:14 -0600

    Add Coin class

--- dispositivos_moviles/android_course/4_1_dice.kt
+++ dispositivos_moviles/android_course/4_1_dice.kt
@@ -4,11 +4,24 @@ fun main() {
     
     val mySecondDice = Dice(20)
     println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+
+    val myCoin = Coin()
+    println("Your coin flipped ${myCoin.flip()}!")
     
 }
 
 class Dice(val numSides: Int = 6) {
     fun roll(): Int {
         return (1..numSides).random()
+    }
+}
+
+class Coin() {
+    fun flip(): String {
+        if ((1..2).random() == 1) {
+            return "Cara"
+        } else {
+            return "Cruz"
+        }
     }
 }
